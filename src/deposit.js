64f38ef436bc24f8701e9eb24779d413dc321ff2
ANANDHAKUMAR M <139849648+anandh0007@users.noreply.github.com>
2024-03-25 17:30:55 +0530

    Client

---
+++ src/deposit.js
@@ -0,0 +1,55 @@
+import { useEffect, useState} from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import axios from 'axios';
+
+export default function Deposit() {
+  const [dep, setDep] = useState(0);
+  const [userId, setUserId] = useState('');
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://server-cebj.onrender.com/data');
+        setData(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  function handleClick(e) {
+    e.preventDefault();
+    let updateData;
+
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].id === Number(userId)) {
+        data[i].amount = Number(data[i].amount) + Number(dep);
+        updateData = { amount: data[i].amount };
+        const url = `https://server-cebj.onrender.com/update/${data[i]._id}`;
+        axios.put(url, updateData);
+        alert(`Rs.${dep} Amount Credited on Your Account`);
+      }
+    }
+  }
+
+  return (
+    <>
+      <Form onSubmit={handleClick} id="deposit">
+        <h4>Deposit Your Amount Here</h4>
+        <hr />
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label id="label">Account No:</Form.Label>
+          <Form.Control type="number" id="input" onChange={(e) => { setUserId(e.target.value) }} />
+          <Form.Label id="label">Deposit Amount:</Form.Label>
+          <Form.Control type="number" id="input" onChange={(e) => { setDep(e.target.value) }} />
+          <Button type="submit" id="submitbtn" variant="danger">Submit</Button>
+          <Button type="reset" id="resetbtn" variant="primary">Reset</Button>
+        </Form.Group>
+      </Form>
+    </>
+  );
+}
